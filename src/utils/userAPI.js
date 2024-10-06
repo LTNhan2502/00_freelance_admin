c0e3536dfc465536aaf95df3fe27f6b90bb042e3
titvvn <chuongtoan1602@gmail.com>
2024-10-06 18:15:22 +0700

    Update

--- src/utils/userAPI.js
+++ src/utils/userAPI.js
@@ -5,10 +5,18 @@ const getUser = () => {
    return instance.get(URL_API);
 }
 
+// Update amout user
+const updateAmountUser = (userId, amount) => {
+   const URL_API = "/v1/api/user";
+   const data = { userId, amount }
+   return instance.post(URL_API, data);
+}
+
+
 // const createUser = (userName, password) => {
 //    const URL_API = "/v1/api/login";
 //    const data = { userName, password }
 //    return instance.post(URL_API, data);
 // }
 
-export { getUser }
+export { getUser, updateAmountUser }
