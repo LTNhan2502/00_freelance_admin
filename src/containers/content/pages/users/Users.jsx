35ecfc84718320eae2d84e2ae4533afc11a268f8
titvvn <chuongtoan1602@gmail.com>
2024-10-06 22:51:57 +0700

    Update

--- src/containers/content/pages/users/Users.jsx
+++ src/containers/content/pages/users/Users.jsx
@@ -1,163 +1,180 @@
-import { Space, Table, Typography, Button, Modal, Input, Form, message } from 'antd';
-import React, { useEffect, useState } from 'react';
-import { EditOutlined, DeleteOutlined, PlusOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
-import { getUser, updateAmountUser } from '../../../../utils/userAPI';
+import {
+  Space,
+  Table,
+  Typography,
+  Button,
+  Modal,
+  Input,
+  Form,
+  message,
+} from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  PlusOutlined,
+  PlusCircleOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { getUser, updateAmountUser } from "../../../../utils/userAPI";
 
 function Users() {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const [dataSource, setDataSource] = useState([]);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [selectedUserID, setSelectedUserID] = useState(null);
-    const [form] = Form.useForm(); 
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedUserID, setSelectedUserID] = useState(null);
+  const [form] = Form.useForm();
 
-    useEffect(() => {
-        getAllUsers();
-    }, []);
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
-    // Fetch users API
-    const getAllUsers = async () => {
-        setLoading(true);
-        const usersData = await getUser();
-        const result = usersData.data.data;
+  // Fetch users API
+  const getAllUsers = async () => {
+    setLoading(true);
+    const usersData = await getUser();
+    const result = usersData.data.data;
 
+    // Handle thêm số 0 cho sđt
+    if (result) {
+      const modifiedData = result.map((item) => ({
+        ...item,
+        numberPhone: "0" + item.numberPhone,
+      }));
+      setDataSource(modifiedData);
+    }
+    setLoading(false);
+  };
 
+  const handleEdit = (id) => {
+    console.log();
+    // navigate(`/products/${id}`);
+  };
 
-        // Handle thêm số 0 cho sđt
-        if (result) {
-            const modifiedData = result.map(item => ({
-                ...item,
-                numberPhone: '0' + item.numberPhone
-            }));
-            setDataSource(modifiedData);
-        }
-        setLoading(false);
-    };
+  const handleDelete = (record) => {
+    console.log("Delete record:", record);
+    // Gọi api delete
+  };
 
-    const handleEdit = (id) => {
-        console.log()
-        // navigate(`/products/${id}`);
-    };
+  // Handle show modal
+  const showModal = (user) => {
+    setSelectedUser(user);
+    setIsModalVisible(true);
+  };
 
-    const handleDelete = (record) => {
-        console.log('Delete record:', record);
-        // Gọi api delete
-    };
+  // Handle submit form rồi đóng form
+  const handleOk = () => {
+    form.submit(); // Kích hoạt submit form khi nhấn OK
+  };
 
-    // Handle show modal
-    const showModal = (user) => {
-        setSelectedUser(user);
-        setIsModalVisible(true);
-    };
+  // Handle cancel form
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    form.resetFields();
+  };
 
-    // Handle submit form rồi đóng form
-    const handleOk = () => {
-        form.submit(); // Kích hoạt submit form khi nhấn OK
-    };
+  // Hanlde nạp tiền
+  const onFinish = async (values) => {
+    const { amount } = values;
+    const userId = selectedUser._id;
+    try {
+      const response = await updateAmountUser(userId, amount);
+      console.log(response);
 
-    // Handle cancel form
-    const handleCancel = () => {
-        setIsModalVisible(false);
-        form.resetFields();
-    };
+      console.log(userId);
+      console.log(amount);
+      message.success(`Nạp tiền thành công cho ${selectedUser.userName}!`);
+      setIsModalVisible(false);
+      form.resetFields();
+    } catch (error) {
+      console.log(error);
+      message.error("Có lỗi xảy ra, vui lòng thử lại!");
+    }
+  };
 
-    // Hanlde nạp tiền
-    const onFinish = async (values) => {
-        const { amount } = values;
-        const userId = selectedUser._id
-        try {
-            const response = await updateAmountUser(userId, amount);
-            console.log(userId);            
-            console.log(amount);            
-            message.success(`Nạp tiền thành công cho ${selectedUser.userName}!`);
-            setIsModalVisible(false);
-            form.resetFields();
-        } catch (error) {
-            message.error('Có lỗi xảy ra, vui lòng thử lại!');
-        }
-    };
+  const handleCreate = () => {
+    navigate("/products/add");
+  };
 
-    const handleCreate = () => {
-        navigate('/products/add');
-    };
-
-    const columns = [
-        {
-            title: "Tên user",
-            dataIndex: "userName",
-        },
-        {
-            title: "Số điện thoại",
-            dataIndex: "numberPhone",  // Use the modified 'numberPhone' from the state
-        },
-        {
-            title: "Mã giới thiệu",
-            dataIndex: "referralCode",
-        },
-        {
-            title: "Action",
-            render: (text, record) => (
-                <Space size="middle">
-                    <Button 
-                        icon={<PlusOutlined />} 
-                        onClick={() => showModal(record)}  // Show modal when clicked, pass the selected user
-                    />
-                    <Button 
-                        icon={<EditOutlined />} 
-                        onClick={() => handleEdit(record.id)} 
-                    />
-                    <Button 
-                        icon={<DeleteOutlined />} 
-                        onClick={() => handleDelete(record)} 
-                        danger 
-                    />
-                </Space>
-            ),
-        },
-    ];
-
-    return (
-        <Space size={20} direction="vertical">
-            <Typography.Title level={4}>Users</Typography.Title>
-            <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-                <Button 
-                    icon={<PlusCircleOutlined />}
-                    size="large"
-                    onClick={handleCreate}
-                >
-                    Thêm User
-                </Button>
-            </div>
-            <Table
-                columns={columns}
-                dataSource={dataSource}
-                loading={loading}
-                rowKey="id"
-            />
-
-            {/* Modal implementation */}
-            <Modal 
-                title={`Thông tin nạp tiền của ${selectedUser?.userName}`} // Hiển thị tên user trong tiêu đề modal
-                visible={isModalVisible} 
-                onOk={handleOk} 
-                onCancel={handleCancel}
-            >
-                <div>
-                    <Form form={form} onFinish={onFinish}>
-                        <Form.Item
-                            name="amount"
-                            label="Số tiền nạp"
-                            rules={[{ required: true, message: 'Vui lòng nhập số tiền!' }]}
-                        >
-                            <Input type="number" placeholder="Nhập số tiền" />
-                        </Form.Item>
-                    </Form>
-                </div>
-            </Modal>
+  const columns = [
+    {
+      title: "Tên user",
+      dataIndex: "userName",
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "numberPhone", // Use the modified 'numberPhone' from the state
+    },
+    {
+      title: "Mã giới thiệu",
+      dataIndex: "referralCode",
+    },
+    {
+      title: "Action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Button
+            icon={<PlusOutlined />}
+            onClick={() => showModal(record)} // Show modal when clicked, pass the selected user
+          />
+          <Button
+            icon={<EditOutlined />}
+            onClick={() => handleEdit(record.id)}
+          />
+          <Button
+            icon={<DeleteOutlined />}
+            onClick={() => handleDelete(record)}
+            danger
+          />
         </Space>
-    );
+      ),
+    },
+  ];
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Users</Typography.Title>
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}
+      >
+        <Button
+          icon={<PlusCircleOutlined />}
+          size="large"
+          onClick={handleCreate}
+        >
+          Thêm User
+        </Button>
+      </div>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        loading={loading}
+        rowKey="id"
+      />
+
+      {/* Modal implementation */}
+      <Modal
+        title={`Thông tin nạp tiền của ${selectedUser?.userName}`} // Hiển thị tên user trong tiêu đề modal
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div>
+          <Form form={form} onFinish={onFinish}>
+            <Form.Item
+              name="amount"
+              label="Số tiền nạp"
+              rules={[{ required: true, message: "Vui lòng nhập số tiền!" }]}
+            >
+              <Input type="number" placeholder="Nhập số tiền" />
+            </Form.Item>
+          </Form>
+        </div>
+      </Modal>
+    </Space>
+  );
 }
 
 export default Users;
