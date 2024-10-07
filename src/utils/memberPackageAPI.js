ae9c4d108c2704d973455d9e9572dfff3c7f6a0f
titvvn <chuongtoan1602@gmail.com>
2024-10-07 17:01:42 +0700

    Update

---
+++ src/utils/memberPackageAPI.js
@@ -0,0 +1,14 @@
+import instance from "../utils/axios.config";
+
+const getAllMemberPackage = () => {
+   const URL_API = "/v1/api/member";
+   return instance.get(URL_API);
+}
+
+const CreateMemberPackage = (packageName, price, distribution, discountFrom, discountTo) => {
+   const URL_API = "/v1/api/member";
+   const data = { packageName, price, distribution, discountFrom, discountTo }
+   return instance.post(URL_API, data);
+};
+
+export { getAllMemberPackage, CreateMemberPackage }
