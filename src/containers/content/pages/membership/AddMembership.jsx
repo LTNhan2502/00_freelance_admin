f5acd485d964005e701d71afe3a5f15bc8e31a5f
LTNhan2502 <firstunio@gmail.com>
2024-10-07 18:04:20 +0700

    update: fetch api, create new membership

--- src/containers/content/pages/membership/AddMembership.jsx
+++ src/containers/content/pages/membership/AddMembership.jsx
@@ -1,32 +1,28 @@
-import { Button, Form, Input, message } from 'antd'
+import { Button, Form, Input, InputNumber, message } from 'antd'
 import React from 'react'
-import { CreateProduct } from '../../../../utils/productAPI'
+import { CreateMemberPackage } from '../../../../utils/memberPackageAPI'
 
 function AddMembership() {
 
+    // Xử lí submit form
     const onFinish = async (values) => {
-        const membership_name = values.membership_name
-        const membership_fee = values.membership_fee
-        const membership_discount_from = values.membership_discount_from
-        const membership_discount_to = values.membership_discount_to
-        const membership_distribution = values.membership_distribution
-
-        const formData = new FormData()
-        formData.append("membership_name", membership_name)
-        formData.append("membership_fee", membership_fee)
-        formData.append("membership_discount_from", membership_discount_from)
-        formData.append("membership_discount_to", membership_discount_to)
-        formData.append("membership_distribution", membership_distribution)
+        // Lấy thông tin từ form
+        const packageName = values.packageName;
+        const price = values.price;
+        const distribution = values.distribution;
+        const discountFrom = values.discountFrom;
+        const discountTo = values.discountTo;
 
         try {
-            // Gọi api tại đây
-            // const result = await CreateProduct(formData)
-            message.success("Thêm mới thành công")
+        // Gọi API với dữ liệu sản phẩm và mảng file ảnh
+        const result = await CreateMemberPackage(packageName, price, distribution, discountFrom, discountTo);
+        message.success("Thêm mới thành công!");
         } catch (error) {
-            console.error("Lỗi khi thêm gói thành viên: ", error);
-            message.error("Thêm mới thất bại")
+        console.error("Lỗi khi thêm sản phẩm:", error);
+        message.error("Thêm mới thất bại!");
         }
-    }
+    };
+    
 
     return (
         <>
@@ -37,61 +33,56 @@ function AddMembership() {
                 style={{ width: "100%", minWidth: "600px" }}
                 onFinish={onFinish}
             >
-                <Form.Item 
-                    label="Tên gói" name="membership_name"
+                <Form.Item label="Tến gói" name="packageName"
                     rules={[
                         {
                             required: true,
                             message: "Không được để trống"
-                        }
+                        },
                     ]}
                 >
-                    <Input/>
+                    <Input />
                 </Form.Item>
 
-                <Form.Item 
-                    label="Phí nâng cấp" name="membership_fee"
+                <Form.Item label="Phí nâng cấp" name="price"
                     rules={[
                         {
                             required: true,
                             message: "Không được để trống"
-                        }
-                    ]}    
+                        },
+                    ]}
                 >
-                    <Input/>
+                    <InputNumber style={{ width: "100%" }} />
                 </Form.Item>
 
-                <Form.Item 
-                    label="Chiết khấu" name="membership_discount" 
-                    rules={[
-                        {
-                            required: true,
-                            message: ""
-                        }
-                    ]}               
+                <Form.Item
+                    label="Chiết khấu"
+                    style={{
+                        marginBottom: 0,
+                    }}
                 >
-                    <Form.Item 
-                        name="membership_discount_from"
+                    <Form.Item
+                        name="discountFrom"
                         rules={[
                             {
                                 required: true,
                                 message: "Không được để trống"
-                            }
+                            },
                         ]}
                         style={{
                             display: 'inline-block',
                             width: 'calc(50% - 8px)',
                         }}
                     >
-                        <Input placeholder='Từ'/>
+                        <InputNumber placeholder="Từ" style={{ width: "100%" }}/>
                     </Form.Item>
-                    <Form.Item 
-                        name="membership_discount_to"
+                    <Form.Item
+                        name="discountTo"
                         rules={[
                             {
                                 required: true,
                                 message: "Không được để trống"
-                            }
+                            },
                         ]}
                         style={{
                             display: 'inline-block',
@@ -99,24 +90,24 @@ function AddMembership() {
                             margin: '0 8px',
                         }}
                     >
-                        <Input placeholder='Đến'/>
+                        <InputNumber placeholder="Đến" style={{ width: "100%" }}/>
                     </Form.Item>
                 </Form.Item>
 
-                <Form.Item
-                    label="Lượt phân phối" name="membership_distribution"
+                <Form.Item label="Lượt phân phối" name="distribution"
                     rules={[
                         {
                             required: true,
                             message: "Không được để trống"
-                        }
+                        },
                     ]}
                 >
-                    <Input/>
+                    <InputNumber style={{ width: "100%" }} />
                 </Form.Item>
 
-                <Form.Item label=" " colon={false}>
-                    <Button type="primary" htmlType="submit" style={{width: "100%"}}>
+                {/* Nút Thêm ở cuối form */}
+                <Form.Item wrapperCol={{ span: 24 }}>
+                    <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
                         Thêm
                     </Button>
                 </Form.Item>
--- src/containers/content/pages/membership/Membership.jsx
+++ src/containers/content/pages/membership/Membership.jsx
@@ -15,121 +15,103 @@ function Membership() {
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
 
-  const columns = [
-    {
-      title: "Tên gói",
-      dataIndex: "membership_name",
-    },
-    {
-      title: "Phí nâng cấp",
-      dataIndex: "membership_fee",
-    },
-    {
-      title: "Chiết khấu",
-      dataIndex: "membership_discount",
-    },
-    {
-      title: "Lượt phân phối",
-      dataIndex: "membership_distribution",
-    },
-    {
-      title: "Action",
-      render: (text, record) => (
-        <Space size="middle">
-          <Button
-            icon={<EyeOutlined />}
-            onClick={() => handleView(record.id)}
-          />
-          <Button
-            icon={<EditOutlined />}
-            onClick={() => handleEdit(record.id)}
-          />
-          <Button
-            icon={<DeleteOutlined />}
-            onClick={() => handleDelete(record)}
-            danger
-          />
+    useEffect(() => {
+        fetchAllMembership();
+    }, []);
+    
+    // Fetch api
+    const fetchAllMembership = async () => {
+        setLoading(true);
+        const getMemberPackages = await getAllMemberPackage();
+        const result = getMemberPackages.data.data
+        
+        if(result && getMemberPackages.data.data){
+            setDataSource(result);
+            setLoading(false);
+        }
+    };
+    
+    const columns = [
+      {
+        title: "Tên gói",
+        dataIndex: "packageName",
+      },
+      {
+        title: "Phí nâng cấp",
+        dataIndex: "price",
+        align: 'center',
+      },
+      {
+        title: "Chiết khấu",
+        align: 'center',
+        render: (text, record) => `${record.discountFrom} - ${record.discountTo}`,
+      },
+      {
+        title: "Lượt phân phối",
+        dataIndex: "distribution",
+        align: 'center',
+      },
+      {
+        title: "Action",
+        align: 'right',
+        render: (text, record) => (
+          <Space size="middle">
+            <Button
+              icon={<EyeOutlined />}
+              onClick={() => handleView(record.id)}
+            />
+            <Button
+              icon={<EditOutlined />}
+              onClick={() => handleEdit(record.id)}
+            />
+            <Button
+              icon={<DeleteOutlined />}
+              onClick={() => handleDelete(record)}
+              danger
+            />
+          </Space>
+        ),
+      },
+    ];
+
+    const handleCreate = () => {
+        navigate("/memberships/add");
+    };
+
+    const handleView = (id) => {
+        console.log("View record:", id);
+    };
+
+    const handleEdit = (id) => {
+        console.log("Edit record:", id);
+    };
+
+    const handleDelete = (record) => {
+        message.success("Xoá thử thành công:", record);
+    };
+
+    return (
+        <Space direction="vertical">
+            <Typography.Title level={4}>Gói thành viên</Typography.Title>
+            <div
+                style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}
+            >
+                <Button
+                    icon={<PlusCircleOutlined />}
+                    size="large"
+                    onClick={() => handleCreate()}
+                >
+                    Thêm gói
+                </Button>
+            </div>
+            <Table
+                columns={columns}
+                dataSource={dataSource}
+                loading={loading}
+                rowKey="_id"
+            ></Table>
         </Space>
-      ),
-    },
-  ];
-
-  // Dữ liệu cứng (mock data)
-  const mockMembershipData = [
-    {
-      id: 1,
-      membership_name: "Bạc",
-      membership_fee: "0 €",
-      membership_discount: "5%",
-      membership_distribution: 10,
-    },
-    {
-      id: 2,
-      membership_name: "Vàng",
-      membership_fee: "1000 €",
-      membership_discount: "10%",
-      membership_distribution: 20,
-    },
-    {
-      id: 3,
-      membership_name: "Bạch kim",
-      membership_fee: "5000 €",
-      membership_discount: "15%",
-      membership_distribution: 30,
-    },
-  ];
-
-  // Giả lập fetch data
-  const fetchAllMembership = async () => {
-    setLoading(true);
-    const getMemberPackages = await getAllMemberPackage();
-    console.log(getMemberPackages);
-    setDataSource(getMemberPackages);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchAllMembership();
-  }, []);
-
-  const handleCreate = () => {
-    navigate("/memberships/add");
-  };
-
-  const handleView = (id) => {
-    console.log("View record:", id);
-  };
-
-  const handleEdit = (id) => {
-    console.log("Edit record:", id);
-  };
-
-  const handleDelete = (record) => {
-    message.success("Xoá thử thành công:", record);
-  };
-
-  return (
-    <Space direction="vertical">
-      <Typography.Title level={4}>Gói thành viên</Typography.Title>
-      <div
-        style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}
-      >
-        <Button
-          icon={<PlusCircleOutlined />}
-          size="large"
-          onClick={() => handleCreate()}
-        >
-          Thêm gói
-        </Button>
-      </div>
-      <Table
-        columns={columns}
-        dataSource={dataSource}
-        loading={loading}
-        rowKey="id"
-      ></Table>
-    </Space>
-  );
+    );
 }
 
 export default Membership;
